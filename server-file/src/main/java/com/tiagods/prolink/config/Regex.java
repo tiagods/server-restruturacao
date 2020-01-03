70140fe85d06538209f2d86d609039ca930751c7
tiagods <tiagoice@hotmail.com>
2020-01-03 00:47:31 -0300

    criando testes das configurações

--- server-file/src/main/java/com/tiagods/prolink/config/Regex.java
+++ server-file/src/main/java/com/tiagods/prolink/config/Regex.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
-
 @Component
 @EnableConfigurationProperties
 @ConfigurationProperties(prefix = "regex")
