1e752b9e46a69b603d9b5fa1c14a2642ca4e27ff
tiagods <tiagoice@hotmail.com>
2020-01-03 00:48:07 -0300

    criando testes das configurações

---
+++ server-file/src/test/java/com/tiagods/springbootfile/config/TesterRegex.java
@@ -0,0 +1,39 @@
+package com.tiagods.springbootfile.config;
+
+import com.tiagods.prolink.config.Regex;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Regex.class)
+public class TesterRegex {
+    @Autowired
+    private Regex regex;
+
+    @Test
+    public void validStructurePath(){
+        Assert.assertTrue("GERAL/SAC".matches(regex.getStructure()));
+    }
+    @Test
+    public void validStructurePathMin3(){
+        Assert.assertFalse("GE".matches(regex.getStructure()));
+    }
+    @Test
+    public void validStructurePathMax20(){
+        Assert.assertFalse("GERAL/MINHAPASTAEXEPLOTESTE/SAC".matches(regex.getStructure()));
+    }
+
+    @Test
+    public void validByInitNickName(){
+        Assert.assertTrue("2222".matches(regex.getInitById()));
+        Assert.assertTrue("2222 ".matches(regex.getInitById()));
+        Assert.assertTrue("2222-".matches(regex.getInitById()));
+        Assert.assertTrue("2222L ".matches(regex.getInitById()));
+        Assert.assertFalse("22223".matches(regex.getInitById()));
+
+    }
+}
---
+++ server-file/src/test/java/com/tiagods/springbootfile/config/TesterServerFile.java
@@ -0,0 +1,30 @@
+package com.tiagods.springbootfile.config;
+
+import com.tiagods.prolink.config.ServerFile;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ServerFile.class)
+public class TesterServerFile {
+    @Autowired
+    private ServerFile serverFile;
+
+    @Test
+    public void testerLoad(){
+        Assert.assertNotNull(serverFile.getBase()) ;
+        Path path = Paths.get(serverFile.getBase());
+        Assert.assertTrue(Files.exists(path));
+    }
+}
